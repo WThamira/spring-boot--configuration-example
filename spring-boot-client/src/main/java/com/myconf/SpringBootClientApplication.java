1f3a9e8602a3446af5836421ae3a6e5a71ed52fa
Thamira Lakmal <thamira@Thamiras-MacBook-Pro.local>
2017-03-13 22:16:36 +0530

    add actuator

--- spring-boot-client/src/main/java/com/myconf/SpringBootClientApplication.java
+++ spring-boot-client/src/main/java/com/myconf/SpringBootClientApplication.java
@@ -1,9 +1,12 @@
 package com.myconf;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
-
+@RefreshScope
+@EnableAutoConfiguration
 @SpringBootApplication
 public class SpringBootClientApplication {
 
